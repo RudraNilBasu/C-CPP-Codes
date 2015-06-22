b0428f9c848393e4aa5821f63887a49662406f4b
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2015-06-23 02:26:39 +0530

    Solution of "Marbles"

---
+++ Competitive-Programming/Codechef/MARBLES.c
@@ -0,0 +1,41 @@
+//http://www.codechef.com/problems/MARBLES
+//nCr = (n*(n-r+1)*(n-r+2)......)/r!
+
+#include<stdio.h>
+
+
+long long int multinomial(long n,long r)
+{
+    long long int i,ans;
+    if(r==n)
+    {
+        return 1;
+    }
+    else
+    {
+        if(r>n/2)
+        {
+            r=n-r;
+        }
+        ans=1;
+        for(i=1;i<=r;i++)
+        {
+            ans*=(n-r+i);
+            ans/=i;
+        }
+        return ans;
+    }
+}
+int main()
+{
+    int t;
+    long k,n;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%ld %ld",&n,&k);
+        printf("%lld\n",multinomial(n-1,k-1));
+    }
+    return 0;
+}
+
