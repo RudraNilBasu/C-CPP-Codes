15d6e251bc0a91072c6f890db3ba9bb4a8311cd4
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2015-10-01 20:34:12 +0530

    C Template

---
+++ Templates/C.c
@@ -0,0 +1,19 @@
+#include<string.h>
+#include<stdlib.h>
+#include<stdio.h>
+#define inpint(x) scanf("%d",&x)
+#define inpllu(x) scanf("%llu",&x)
+#define input(x) scanf("%s",x)
+#define RUNBF(i,a,b) for(i=a;i<b;i++)
+#define RUN(i,a,b) for(i=a;i<=b;i++)
+#define newline printf("\n")
+#define printc(x) printf("%c",x)
+#define printllu(x) printf("%llu\n",x)
+#define RNB printf("Done by : Rudra Nil Basu\n")
+#define llu unsigned long long int
+#define MAX(a,b) ((a)>(b))?(a):(b)
+
+int main()
+{
+    return 0;
+}
