961d07dac44d2e9ea7a70b0a02e17075d062e27a
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2015-10-07 21:35:04 +0530

    Update C.c

--- Templates/C.c
+++ Templates/C.c
@@ -1,19 +1,21 @@
-#include<string.h>
-#include<stdlib.h>
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #define inpd(x) scanf("%d",&x)
-#define inpllu(x) scanf("%llu",&x)
-#define inplld(x) scanf("%lld",&x)
 #define inpld(x) scanf("%ld",&x)
+#define inplld(x) scanf("%lld",&x)
 #define input(x) scanf("%s",x)
-#define RUNBF(i,a,b) for(i=a;i<b;i++)
 #define RUN(i,a,b) for(i=a;i<=b;i++)
-#define newline printf("\n")
+#define RUNBF(i,a,b) for(i=a;i<b;i++)
+#define llu long long int
 #define printc(x) printf("%c",x)
-#define printllu(x) printf("%llu\n",x)
-#define RNB printf("Done by : Rudra Nil Basu\n")
-#define llu unsigned long long int
+#define printd(x) printf("%d",x)
+#define printld(x) printf("%ld",x)
+#define printlld(x) printf("%lld",x)
+#define newline printf("\n")
 #define MAX(a,b) ((a)>(b))?(a):(b)
+#define RNB printf("Done by : Rudra Nil Basu\n")
+
 
 int main()
 {
