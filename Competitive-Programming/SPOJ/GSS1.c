91d923964f982b10ce9e9bed495fe52dd21ed82b
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2016-01-21 01:43:56 +0530

    Solution of GSS1

--- Competitive-Programming/SPOJ/GSS1.c
+++ Competitive-Programming/SPOJ/GSS1.c
@@ -1,84 +1,92 @@
-//http://www.spoj.com/problems/GSS1/
-#include <stdio.h>
-
-typedef struct{
-    int sum;
-    int best;
+#include<stdio.h>
+typedef struct
+{
     int left;
     int right;
-}element;
-
+    int best;
+    int sum;
+} element;
 short int arr[50000];
 element M[131200];
-
 inline int max(int a,int b)
 {
     return (a>b)?a:b;
 }
-
-element combine(element *l,element *r)
+element combine(element *l, element *r)
 {
+    //printf("2\n");
     element e;
-    e.left = l->left;
-    if(l->sum+r->left>e.left)
-        e.left = l->sum + r->left;
-    e.right = r->right;
+    e.left=l->left;
+    if(l->sum+r->left > e.left)
+    {
+        e.left = l->sum+r->left;
+    }
+    e.right=r->right;
     if(r->sum+l->right>e.right)
-        e.right = r->sum + l->right;
-    e.best = max(l->right+r->left,max(l->best,r->best));
-    e.sum = l->sum + r->sum;
+    {
+        e.right=r->sum+l->right;
+    }
+    e.best=max(l->right+r->left,max(l->best,r->best));
+    e.sum=l->sum+r->sum;
     return e;
 }
-
-void initialise(int node,int b,int e)
+element query(int si,int ss,int se,int qs,int qe)
 {
-    if(b==e)
+    //printf("1\n");
+    if(qs<=ss && se<=qe)
     {
-        M[node].sum = arr[b];
-        M[node].right = arr[b];
-        M[node].left = arr[b];
-        M[node].best = arr[b];
+        return M[si];
     }
-    else{
-        int mid = (b+e)>>1;
-        int left_child = 2*node;
-        int right_child = 2*node+1;
-        initialise(left_child,b,mid);
-        initialise(right_child,mid+1,e);
-        M[node] = combine(&M[left_child],&M[right_child]);
+    int mid=(ss+se)>>1;
+    int left_child = si<<1;
+    int right_child = (si<<1)+1;
+    if(qe<=mid)
+    {
+        return query(left_child,ss,mid,qs,qe);
     }
-}
-
-element query(int node,int b,int e,int l,int r)
-{
-    if((l<=b)&&(e<=r))
-        return M[node];
-    int mid = (b+e)>>1;
-    int left_child = node<<1;
-    int right_child = (node<<1)+1;
-    if(r<=mid)
-        return query(left_child,b,mid,l,r);
-    if(l>mid)
-        return query(right_child,mid+1,e,l,r);
-    element p = query(left_child,b,mid,l,r);
-    element q = query(right_child,mid+1,e,l,r);
+    if(qs>mid)
+    {
+        return query(right_child,mid+1,se,qs,qe);
+    }
+    element p=query(left_child,ss,mid,qs,qe);;
+    element q=query(right_child,mid+1,se,qs,qe);
     return combine(&p,&q);
 }
-
-int main(void)
+int construct(int si,int ss,int se)
 {
-    int i,n,m,x,y;
-    element e;
+    if(ss==se)
+    {
+        M[si].sum=arr[ss];
+        M[si].left=arr[ss];
+        M[si].right=arr[ss];
+        M[si].best=arr[ss];
+    }
+    else
+    {
+        int mid=(ss+se)>>1;
+        int left_child=2*si;
+        int right_child=2*si+1;
+        construct(left_child,ss,mid);
+        construct(right_child,mid+1,se);
+        M[si]=combine(&M[left_child],&M[right_child]);
+    }
+}
+int main()
+{
+    int n,i,m,qs,qe;
     scanf("%d",&n);
     for(i=0;i<n;i++)
-        scanf("%hd",&arr[i]);
-    initialise(1,0,n-1);
-    scanf("%d",&m);
-    for(i=0;i<m;i++)
     {
-        scanf("%d %d",&x,&y);
-        e = query(1,0,n-1,x-1,y-1);
+        scanf("%hd",&arr[i]);
+    }
+    construct(1,0,n-1);
+    scanf("%d",&m);
+    while(m--)
+    {
+        scanf("%d %d",&qs,&qe);
+        element e=query(1,0,n-1,qs-1,qe-1);
         printf("%d\n",e.best);
     }
     return 0;
 }
+
