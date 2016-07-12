b5c0288dcb9e2ef757ac8775e0c5e4a848c88ebe
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2016-07-12 21:30:47 +0530

    Update RENT.cpp

--- Competitive-Programming/SPOJ/RENT.cpp
+++ Competitive-Programming/SPOJ/RENT.cpp
@@ -28,7 +28,6 @@ ll calc(int start, int end, ticket disc[])
 		if(disc[i].start>=pos)
 			break;
 	}
-	//printf("Considering %d, start pos %d\n",disc[start].start,pos);
 	return dp[start][end] = max(
 			disc[start].price+calc(i,end,disc),
 			calc(start+1,end,disc)
@@ -39,7 +38,6 @@ int main()
 	int t,i,j;
 	scanf("%d",&t);
 	while(t--) {
-		//int n;
 		memset(dp,-1,sizeof(dp));
 		scanf("%d",&n);
 		ticket disc[n];
@@ -47,21 +45,6 @@ int main()
 			scanf("%d %d %d",&disc[i].start,&disc[i].duration,&disc[i].price);
 		}
 		sort(disc,disc+n,cmp);
-		//sort(disc,disc+n);
-		/*
-		for(i=0;i<n-1;i++) {
-			for(j=0;j<n-i-1;j++) {
-				if( disc[j].start>disc[j+1].start ) {
-					ticket temp=disc[j+1];
-					disc[j+1]=disc[j];
-					disc[j]=temp;
-				}
-			}
-		}*/
-		/*
-		for(i=0;i<n;i++) {
-			printf("%d\t%d\t%d\n",disc[i].start,disc[i].duration,disc[i].price);
-		}*/
 		printf("%lld\n",calc(0,n-1,disc));
 	}
 	return 0;
