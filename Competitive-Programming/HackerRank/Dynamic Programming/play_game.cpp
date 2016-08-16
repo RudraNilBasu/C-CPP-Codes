898b86359eb1a491635c39d59a8315f1206117b8
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2016-08-16 13:11:26 +0530

    Play Game

--- Competitive-Programming/HackerRank/Dynamic Programming/play_game.cpp
+++ Competitive-Programming/HackerRank/Dynamic Programming/play_game.cpp
@@ -1,10 +1,14 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 #define ll long long int
 using namespace std;
 
 int n;
 ll *a;
+
+ll dp[2][100001];
 
 ll max2(ll a, ll b)
 {
@@ -18,6 +22,17 @@ ll max3(ll a, ll b, ll c)
 	return max2( a, max2(b,c) );
 }
 
+ll min2(ll a, ll b)
+{
+	if(a<=b)
+		return a;
+	return b;
+}
+
+ll min3(ll a, ll b, ll c)
+{
+	return min2( a, min2(b,c) );
+}
 
 ll maxScore(int player, int index)
 {
@@ -27,40 +42,43 @@ ll maxScore(int player, int index)
 		return 0;
 	}
 
+	if(dp[player][index]!=-1)
+		return dp[player][index];
+
 	if(index==(n-1))
 	{
 		if(player) {
-			return a[n-1];
+			return dp[player][index]=a[n-1];
 		} else {
-			return 0;
+			return dp[player][index]=0;
 		}
 	}
 	if(index==(n-2))
 	{
 		if(player)
-		return a[n-2]+a[n-1];
+		return dp[player][index]= a[n-2]+a[n-1];
 		else 
-			return 0;
+			return dp[player][index]=0;
 	}
 	if(index==(n-3))
 	{
 		if(player)
-		return a[n-3]+a[n-2]+a[n-1];
+		return dp[player][index]=a[n-3]+a[n-2]+a[n-1];
 		else 
-			return =0;
+			return dp[player][index]=0;
 	}
 
 	//printf("Player %d Index %d\n",player, index);
 	if(player) {
-		return 
+		return dp[player][index]=
 			max3(
 				a[index]+maxScore(!player, index+1),
 				a[index]+a[index+1]+maxScore(!player, index+2),
 				a[index]+a[index+1]+a[index+2]+maxScore(!player, index+3)
 			   );
 	} else {
-		return 
-			max3(
+		return dp[player][index]=
+			min3(
 				maxScore(!player, index+1),
 				maxScore(!player, index+2),
 				maxScore(!player, index+3)
@@ -73,14 +91,15 @@ int main()
 	int t,i;
 	scanf("%d",&t);
 	while(t--) {
+		memset(dp,-1,sizeof(dp));
 		scanf("%d",&n);
 		a = new ll[n];
 		for(i=0;i<n;i++) {
 			scanf("%lld",&a[i]);
 		}
 		printf("%lld\n",maxScore(1,0));
-		printf("test %lld\n",maxScore(0,1));
-		printf("HUe %lld\n",max3( maxScore(0,1), maxScore(0,2), maxScore(0,3)  ));
+		//printf("test %lld\n",maxScore(0,1));
+		//printf("HUe %lld\n",max3( maxScore(0,1), maxScore(0,2), maxScore(0,3)  ));
 	}
 	return 0;
 }
