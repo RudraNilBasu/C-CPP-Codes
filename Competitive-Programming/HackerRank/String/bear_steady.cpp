8d8c8202c36c3c2ccf49bce74a6217153692877d
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2016-09-10 11:28:33 +0530

    bear_steady

--- Competitive-Programming/HackerRank/String/bear_steady.cpp
+++ Competitive-Programming/HackerRank/String/bear_steady.cpp
@@ -38,7 +38,7 @@ int main()
 	for(i=0;i<4;i++) {
 		printf("%c\t%d\n",genes[i],letter_count[genes[i]]);
 	}
-	*/
+*/	
 	int start=0,end=0,min_length=n;
 	map<char,int> substring; // stores freq of characters
 	//in the substring [start....end)
