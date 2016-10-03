0681b869685505f259f97d783e02932f18c02f55
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2016-10-03 11:45:52 +0530

    374-Div#2 prob B

--- Competitive-Programming/Codeforces/374-Div2/b.cpp
+++ Competitive-Programming/Codeforces/374-Div2/b.cpp
@@ -17,6 +17,15 @@ int max(int a, int b)
 
 using namespace std;
 
+int calc(int count, int k)
+{
+	if(count%k==0) {
+		return 5*((count-1)/k);
+	} else {
+		return 5*(count/k);
+	}
+}
+
 int main()
 {
 	int n,k;
@@ -34,7 +43,7 @@ int main()
 	sort(pass,pass+n);
 	// no of numbers before passLen in pass
 	// no of passLen's in pass
-	int count=0;
+	int count=0; // no of passwords of len < len of real pass
 	int j=-1;
 	for(i=0;i<n;i++) {
 		if(pass[i]>passLen) {
@@ -46,15 +55,26 @@ int main()
 		}
 		count++;
 	}
-	int count2=0;
+	int count2=0; // no of pass of len = len of real pass
+	/*
 	while(j!=-1 && j<n && pass[j]==passLen) {
 		count2++;
 		j++;
 	}
+	*/
+	for(i=0;i<n;i++) {
+		if(pass[i]==passLen) {
+			count2++;
+		} else if(pass[i]>passLen) {
+			break;
+		}
+	}
 	//printf("%d %d\n",count,count2);
-	int ans=count+5*(count/k);
-	printf("%d ",ans+1);
-	ans+=count2+5*(count2/k);
-	printf("%d\n",ans);
+	//int ans1=(count)+5*((count+1)/k) + 1;
+	int ans1=(count+1) + calc(count+1,k);
+	printf("%d ",ans1);
+	//int ans2=count + calc(count, k);
+	int ans2=(count2+count)+calc(count+count2,k);
+	printf("%d\n",ans2);
 	return 0;
 }
