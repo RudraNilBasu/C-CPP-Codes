fb177d201f9260c048841f29bb963725167d7593
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2016-11-26 03:08:48 +0530

    Vertical

--- Competitive-Programming/Codechef/NOV16/URBANDEV_2.cpp
+++ Competitive-Programming/Codechef/NOV16/URBANDEV_2.cpp
@@ -55,7 +55,7 @@ typedef struct vert {
 
 typedef struct SegmentTreeNode {
 	int no; // no of 1 in the range
-	void assignValue(int num)
+	void assignLeaf(int num)
 	{
 		no=num;
 	}
@@ -71,7 +71,72 @@ typedef struct SegmentTreeNode {
 
 class SegmentTree 
 {
-	//
+	SegmentTreeNode* nodes;
+	int N;
+public:
+	SegmentTree(int N)
+	{
+		this->N=N;
+		nodes=new SegmentTreeNode[getSegmentTreeSize(N)];
+		buildTree(1,0,N-1);
+	}
+	int getValue(int lo, int hi)
+	{
+		SegmentTreeNode result=getValue(1,0,N-1,lo,hi);
+		return result.getValue();
+	}
+	void update(int index, int value)
+	{
+		update(1,0,N-1, index, value);
+	}
+private:
+	void buildTree(int stIndex, int lo, int hi)
+	{
+		if(lo==hi) {
+			nodes[stIndex].assignLeaf(0);
+			return;
+		}
+		int left=2*stIndex,right=left+1,mid=(lo+hi)/2;
+		buildTree(left, lo, mid);
+		buildTree(right, mid+1, hi);
+		nodes[stIndex].merge(nodes[left], nodes[right]);
+	}
+	SegmentTreeNode getValue(int stIndex, int left, int right, int lo, int hi)
+	{
+		if(left==lo && right==hi) {
+			return nodes[stIndex];
+		}
+		int mid=(left+right)/2;
+		if (lo > mid)
+			return getValue(2*stIndex+1, mid+1, right, lo, hi);
+		if (hi <= mid)
+			return getValue(2*stIndex, left, mid, lo, hi);
+
+		SegmentTreeNode leftResult = getValue(2*stIndex, left, mid, lo, mid);
+		SegmentTreeNode rightResult = getValue(2*stIndex+1, mid+1, right, mid+1, hi);
+		SegmentTreeNode result;
+		result.merge(leftResult, rightResult);
+		return result;
+	}
+	int getSegmentTreeSize(int N) {
+		int size = 1;
+		for (; size < N; size <<= 1);
+		return size << 1;
+	}
+	void update(int stIndex, int lo, int hi, int index, int value) {
+		if (lo == hi) {
+			nodes[stIndex].assignLeaf(value);
+			return;
+		}
+
+		int left = 2 * stIndex, right = left + 1, mid = (lo + hi) / 2;
+		if (index <= mid)
+			update(left, lo, mid, index, value);
+		else
+			update(right, mid+1, hi, index, value);
+
+		nodes[stIndex].merge(nodes[left], nodes[right]);
+	}
 };
 
 int main()
@@ -102,26 +167,48 @@ int main()
 			//cout<<"Horizontal pushed\n";
 		}
 	}
+	int ans=0;
+	int city[n+1];
+	memset(city, 0, sizeof(city));
 	//int sweep[100001]; // the sweeping line
 	for(i=0;i<v.size();i++) {
 		int x_corr=v[i].x;
 		int _y1=v[i].y1;
-		int _y2=[i].y2;
+		int _y2=v[i].y2;
 		// sweep array creation for this vertical segment
 		// create the array sweep[i]=1 when intersects
 		//memset(sweep, 0, sizeof(sweep));
 		// create seg tree
+		SegmentTree st(100001);
 		for(j=0;j<h.size();j++) {
 			int _x1=h[j].x1;
 			int _x2=h[j].x2;
 			// update seg tree value of h[j].y to 1
 			if(x_corr>=_x1 && x_corr<=_x2) {
+				// check corner case (round)
 				//sweep[h[j].y]=1;
 				// update seg tree value of h[j].y to 1
+				st.update(h[j].y,1);
 			}
 		}
 		// query for range _y1 to _y2
+		int x=st.getValue(_y1, _y2);
+		ans+=x;
+		city[v[i].no]=x;
 	}
+	for(i=0;i<h.size();i++) {
+		int y_corr=h[i].y;
+		int _x1=h[i].x1;
+		int _x2=h[i].x2;
+		for(j=0;j<v.size();j++) {
+			//
+		}
+	}
+	cout<<ans<<endl;
+	for(i=1;i<=n;i++) {
+		cout<<city[i]<<" ";
+	}
+	cout<<"\n";
 	return 0;
 }
 
