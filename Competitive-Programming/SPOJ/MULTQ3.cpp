35cdfd7effc6ea7fc93c0480e113f83e4a26ab9d
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2016-11-27 12:31:46 +0530

    MULTQ3 non lazy

--- Competitive-Programming/SPOJ/MULTQ3.cpp
+++ Competitive-Programming/SPOJ/MULTQ3.cpp
@@ -1,13 +1,27 @@
 #include <cstdio>
 #include <algorithm>
+#include <stdlib.h>
+#include <string.h>
+
 using namespace std;
 
 struct SegmentTreeNode {
 	int val;
+	int n; // current value of the leaf node
 	
 	void assignLeaf(int num) {
-		//n=num;
+		n=num;
 		if(num%3==0) {
+			val=1;
+		} else {
+			val=0;
+		}
+	}
+
+	void add(int value)
+	{
+		n+=value;
+		if(n%3==0) {
 			val=1;
 		} else {
 			val=0;
@@ -15,52 +29,53 @@ struct SegmentTreeNode {
 	}
 	
 	void merge(SegmentTreeNode& left, SegmentTreeNode& right) {
-		//maxNum = max(left.maxNum, right.maxNum);
-		//secondMaxNum = min(max(left.maxNum, right.secondMaxNum), max(right.maxNum, left.secondMaxNum));
 		val=left.val+right.val;
 	}
 	
 	int getValue() {
-		//return maxNum + secondMaxNum;
 		return val;
 	}
 };
 
-template<class T, class V>
+//template<class T, class V>
 class SegmentTree {
 	SegmentTreeNode* nodes;
+	int *lazy;
 	int N;
 	
 public:
-	SegmentTree(T arr[], int N) {
+	SegmentTree(int N) {
 		this->N = N;
-		nodes = new SegmentTreeNode[getSegmentTreeSize(N)];
-		buildTree(arr, 1, 0, N-1);
+		int sz=getSegmentTreeSize(N);
+		nodes = new SegmentTreeNode[sz];
+		lazy = new int[sz];
+		memset(lazy, 0, sizeof(lazy));
+		buildTree(1, 0, N-1);
 	}
 	
 	~SegmentTree() {
 		delete[] nodes;
 	}
 	
-	V getValue(int lo, int hi) {
+	int getValue(int lo, int hi) {
 		SegmentTreeNode result = getValue(1, 0, N-1, lo, hi);
 		return result.getValue();
 	}
 	
-	void update(int index, T value) {
-		update(1, 0, N-1, index, value);
+	void update(int l, int r, int value) {
+		update(1, 0, N-1, l, r, value);
 	}
 	
 private:	
-	void buildTree(T arr[], int stIndex, int lo, int hi) {
+	void buildTree(int stIndex, int lo, int hi) {
 		if (lo == hi) {
-			nodes[stIndex].assignLeaf(arr[lo]);
+			nodes[stIndex].assignLeaf(0);
 			return;
 		}
 		
 		int left = 2 * stIndex, right = left + 1, mid = (lo + hi) / 2;
-		buildTree(arr, left, lo, mid);
-		buildTree(arr, right, mid + 1, hi);
+		buildTree(left, lo, mid);
+		buildTree(right, mid + 1, hi);
 		nodes[stIndex].merge(nodes[left], nodes[right]);
 	}
 	
@@ -87,64 +102,62 @@ private:
 		return size << 1;
 	}
 	
-	void update(int stIndex, int lo, int hi, int index, T value) {
+	void update(int stIndex, int lo, int hi, int l, int r, int value) {
+		// out of range
+		if(lo>hi || lo>r ||hi<l) {
+			return;
+		}
 		if (lo == hi) {
-			nodes[stIndex].assignLeaf(value);
+			//nodes[stIndex].assignLeaf(value+);
+			nodes[stIndex].add(value);
 			return;
 		}
 		
 		int left = 2 * stIndex, right = left + 1, mid = (lo + hi) / 2;
-		if (index <= mid)
-			update(left, lo, mid, index, value);
+		/*
+		if (stIndex <= mid)
+			update(left, lo, mid, l, r, value);
 		else
-			update(right, mid+1, hi, index, value);
+			update(right, mid+1, hi, l, r, value);
 		
+		nodes[stIndex].merge(nodes[left], nodes[right]);
+		*/
+		update(left, lo, mid, l, r, value);
+		update(right, mid+1, hi, l, r, value);
 		nodes[stIndex].merge(nodes[left], nodes[right]);
 	}
 };
 
 int main() {
-	/*
-	int N, M, x, y, A[100005];
-	char operation[5];
-	
-	scanf("%d", &N);
-	for (int i = 0; i < N; ++i)
-		scanf("%d", &A[i]);
-	SegmentTree<int,int> st(A, N);
-	
-	scanf("%d", &M);
-	while(M--) {
-		scanf("%s %d %d", &operation, &x, &y);
-		if (operation[0] == 'U')
-			st.update(x-1, y);
-		else
-			printf("%d\n", st.getValue(x-1, y-1));
-	}
-	*/
 	int n,q,i;
 	scanf("%d %d",&n,&q);
 	int a[n];
 	for(i=0;i<n;i++) {
 		a[i]=0;
 	}
-	SegmentTree<int,int> st(a,n);
+	//SegmentTree<int,int> st(a,n);
+	SegmentTree st(n);
 	int op,x,y;
 	while(q--) {
 		scanf("%d %d %d",&op,&x,&y);
 		if(op==0) {
+			st.update(x, y, 1);
+			/*
 			for(i=x;i<=y;i++) {
 				st.update(i,1);
 				a[i]++;
 			}
+			*/
 		} else {
 			printf("%d\n",st.getValue(x,y));
 		}
+		/*
 		printf("----------\n");
 		for(i=0;i<n;i++) {
 			printf("%d\n",a[i]);
 		}
 		printf("----------\n");
+		*/
 	}
 	return 0;
 }
