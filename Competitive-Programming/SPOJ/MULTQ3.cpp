138abea24de19a9006b453597228e3fd2b13eac7
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2016-11-27 19:49:45 +0530

    MULTQ3 with lazy

--- Competitive-Programming/SPOJ/MULTQ3.cpp
+++ Competitive-Programming/SPOJ/MULTQ3.cpp
@@ -8,7 +8,8 @@ using namespace std;
 struct SegmentTreeNode {
 	int val;
 	int n; // current value of the leaf node
-	
+	int rem1,rem2,rem0; // how many no s gives remainder 
+	// 0, 1 or 2 on dividing by 3
 	void assignLeaf(int num) {
 		n=num;
 		if(num%3==0) {
@@ -16,28 +17,59 @@ struct SegmentTreeNode {
 		} else {
 			val=0;
 		}
+		if(n%3==0) {
+			rem0=1;
+			rem1=0;
+			rem2=0;
+		} else if(n%3==1) {
+			rem0=0;
+			rem1=1;
+			rem2=0;
+		} else if(n%3==2) {
+			rem0=0;
+			rem1=0;
+			rem2=1;
+		}
+
 	}
 
 	void add(int value)
 	{
+		// add value of leaf
 		n+=value;
 		if(n%3==0) {
 			val=1;
 		} else {
 			val=0;
 		}
+		if(n%3==0) {
+			rem0=1;
+			rem1=0;
+			rem2=0;
+		} else if(n%3==1) {
+			rem0=0;
+			rem1=1;
+			rem2=0;
+		} else if(n%3==2) {
+			rem0=0;
+			rem1=0;
+			rem2=1;
+		}
 	}
 	
 	void merge(SegmentTreeNode& left, SegmentTreeNode& right) {
 		val=left.val+right.val;
+		rem0=left.rem0+right.rem0;
+		rem1=left.rem1+right.rem1;
+		rem2=left.rem2+right.rem2;
 	}
 	
 	int getValue() {
-		return val;
+		return rem0;
+		//return val;
 	}
 };
 
-//template<class T, class V>
 class SegmentTree {
 	SegmentTreeNode* nodes;
 	int *lazy;
@@ -80,6 +112,33 @@ private:
 	}
 	
 	SegmentTreeNode getValue(int stIndex, int left, int right, int lo, int hi) {
+		if(lazy[stIndex]!=0) {
+			// if not leaf node
+			if(lo!=hi) {
+				int left=2*stIndex;
+				int right=left+1;
+				lazy[left]=(lazy[left]+lazy[stIndex])%3;
+				lazy[right]=(lazy[right]+lazy[stIndex])%3;
+			}
+			if(lazy[stIndex]==1) {
+				// shift by one place
+				int _a=nodes[stIndex].rem0;
+				int _b=nodes[stIndex].rem1;
+				int _c=nodes[stIndex].rem2;
+				nodes[stIndex].rem1=_a;
+				nodes[stIndex].rem2=_b;
+				nodes[stIndex].rem0=_c;
+			} else if(lazy[stIndex]==2) {
+				// shift by 2 places
+				int _a=nodes[stIndex].rem0;
+				int _b=nodes[stIndex].rem1;
+				int _c=nodes[stIndex].rem2;
+				nodes[stIndex].rem1=_c; // b
+				nodes[stIndex].rem2=_a; // c
+				nodes[stIndex].rem0=_b; // a
+			}
+			lazy[stIndex]=0;
+		}
 		if (left == lo && right == hi)
 			return nodes[stIndex];
 			
@@ -103,25 +162,60 @@ private:
 	}
 	
 	void update(int stIndex, int lo, int hi, int l, int r, int value) {
+		if(lazy[stIndex]!=0) {
+			// if not leaf node
+			if(lo!=hi) {
+				int left=2*stIndex;
+				int right=left+1;
+				lazy[left]=(lazy[left]+lazy[stIndex])%3;
+				lazy[right]=(lazy[right]+lazy[stIndex])%3;
+			}
+			if(lazy[stIndex]==1) {
+				// shift by one place
+				int _a=nodes[stIndex].rem0;
+				int _b=nodes[stIndex].rem1;
+				int _c=nodes[stIndex].rem2;
+				nodes[stIndex].rem1=_a;
+				nodes[stIndex].rem2=_b;
+				nodes[stIndex].rem0=_c;
+			} else if(lazy[stIndex]==2) {
+				// shift by 2 places
+				int _a=nodes[stIndex].rem0;
+				int _b=nodes[stIndex].rem1;
+				int _c=nodes[stIndex].rem2;
+				nodes[stIndex].rem1=_c; // b
+				nodes[stIndex].rem2=_a; // c
+				nodes[stIndex].rem0=_b; // a
+			}
+			lazy[stIndex]=0;
+		}
 		// out of range
 		if(lo>hi || lo>r ||hi<l) {
 			return;
 		}
+		if(lo>=l && hi<=r) {
+			if(lo!=hi) {
+				// not leaf node
+				// do lazy
+				lazy[stIndex*2]=(1+lazy[stIndex*2])%3;
+				lazy[(stIndex*2)+1]=(1+lazy[(stIndex*2)+1])%3;
+			}
+			int _a=nodes[stIndex].rem0;
+			int _b=nodes[stIndex].rem1;
+			int _c=nodes[stIndex].rem2;
+			nodes[stIndex].rem1=_a;
+			nodes[stIndex].rem2=_b;
+			nodes[stIndex].rem0=_c;
+			return;
+		}
+		/*
 		if (lo == hi) {
-			//nodes[stIndex].assignLeaf(value+);
 			nodes[stIndex].add(value);
 			return;
 		}
+		*/
 		
 		int left = 2 * stIndex, right = left + 1, mid = (lo + hi) / 2;
-		/*
-		if (stIndex <= mid)
-			update(left, lo, mid, l, r, value);
-		else
-			update(right, mid+1, hi, l, r, value);
-		
-		nodes[stIndex].merge(nodes[left], nodes[right]);
-		*/
 		update(left, lo, mid, l, r, value);
 		update(right, mid+1, hi, l, r, value);
 		nodes[stIndex].merge(nodes[left], nodes[right]);
@@ -131,33 +225,15 @@ private:
 int main() {
 	int n,q,i;
 	scanf("%d %d",&n,&q);
-	int a[n];
-	for(i=0;i<n;i++) {
-		a[i]=0;
-	}
-	//SegmentTree<int,int> st(a,n);
 	SegmentTree st(n);
 	int op,x,y;
 	while(q--) {
 		scanf("%d %d %d",&op,&x,&y);
 		if(op==0) {
 			st.update(x, y, 1);
-			/*
-			for(i=x;i<=y;i++) {
-				st.update(i,1);
-				a[i]++;
-			}
-			*/
 		} else {
 			printf("%d\n",st.getValue(x,y));
 		}
-		/*
-		printf("----------\n");
-		for(i=0;i<n;i++) {
-			printf("%d\n",a[i]);
-		}
-		printf("----------\n");
-		*/
 	}
 	return 0;
 }
