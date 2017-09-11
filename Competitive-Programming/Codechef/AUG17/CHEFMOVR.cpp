fcc6a907d2a2cad0f12a81716d35b0616d82a5f6
Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
2017-09-12 00:21:11 +0530

    CHEFMOVR AC/TLE
    Signed-off-by: Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>

--- Competitive-Programming/Codechef/AUG17/CHEFMOVR.cpp
+++ Competitive-Programming/Codechef/AUG17/CHEFMOVR.cpp
@@ -1,20 +1,25 @@
 #include <stdio.h>
+#include <limits.h>
+#include <vector>
+#include <stdlib.h>
 
+#define ll long long int
+#define MAX LLONG_MAX
 using namespace std;
 
 int main()
 {
-        int t, n, d, i;
+        int t, n, d, i, j;
         scanf("%d", &t);
         while (t--) {
                 scanf("%d %d", &n, &d);
-                int a[n];
-                int orig[n];
-                int sum = 0;
+                ll a[n];
+                ll orig[n];
+                ll sum = 0;
                 for (i = 0; i < n; i++) {
-                        scanf("%d", (a + i));
+                        scanf("%lld", (a + i));
                         orig[i] = a[i];
-                        sum += a[i];
+                        sum = (sum % MAX + a[i] % MAX) % MAX;
                 }
                 if (sum % n != 0) {
                         printf("-1\n");
@@ -23,6 +28,79 @@ int main()
 
                 sum /= n;
 
+                vector<ll> g[d];
+                ll sub_sum;
+                int flag = 0;
+                for (i = 0; i < d; i++) {
+                        j = i;
+                        sub_sum = 0;
+                        while (j < n) {
+                                g[i].push_back(a[j]);
+                                sub_sum = (sub_sum % MAX + a[j] % MAX) % MAX;
+                                j += d;
+                        }
+                        if ((sub_sum % g[i].size() != 0) || (sub_sum/g[i].size() != sum)) {
+                                flag = 1;
+                                break;
+                        }
+                }
+                if (flag) {
+                        printf("-1\n");
+                        continue;
+                }
+
+                ll minStep = 0;
+                for (i = 0; i < d; i++) {
+                        for (j = 0; j < g[i].size(); j++) {
+                                if (g[i][j] == sum) {
+                                        continue;
+                                } else if (g[i][j] < sum) {
+                                        // find right index with g[i][j] > sum
+                                        int right = j + 1;
+                                        while (right < g[i].size()) {
+                                                if (g[i][right] > sum) {
+                                                        break;
+                                                }
+                                                right++;
+                                        }
+                                        int left = j - 1;
+                                        while (left >= 0) {
+                                                if (g[i][left] > sum) {
+                                                        break;
+                                                }
+                                                left--;
+                                        }
+                                        int fetch_index;
+                                        if (left < 0) {
+                                                // left is invalid, use right
+                                                fetch_index = right;
+                                        } else if (right >= g[i].size()) {
+                                                fetch_index = left;
+                                        } else {
+                                                if ((right - j) < (left - j)) {
+                                                        fetch_index = right;
+                                                } else {
+                                                        fetch_index = left;
+                                                }
+                                        }
+                                        // send from index
+                                        ll diff_index = g[i][fetch_index] - sum;
+                                        ll diff_curr = sum - g[i][j];
+                                        if (diff_index >= diff_curr) {
+                                                g[i][j] += diff_curr;
+                                                g[i][fetch_index] -= diff_curr;
+                                                minStep += (diff_curr * abs(fetch_index - j));
+                                        } else {
+                                                g[i][j] += diff_index;
+                                                g[i][fetch_index] -= diff_index;
+                                                minStep += (diff_index * abs(fetch_index - j));
+                                                j--;
+                                        }
+                                }
+                        }
+                }
+                printf("%lld\n", minStep);
+                /*
                 int leftStep = 0, rightStep = 0;
                 for (i = 0; i < n; i++) {
                         // [0, i) is balanced
@@ -95,6 +173,7 @@ int main()
                 } else {
                         printf("%d\n", ((leftStep < rightStep) ? leftStep : rightStep));
                 }
+        */
         }
         return 0;
 }
