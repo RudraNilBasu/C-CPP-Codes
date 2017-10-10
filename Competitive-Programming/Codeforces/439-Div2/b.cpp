08824b74f61d616eddb76281d2cbb08eed53c7ea
Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
2017-10-10 19:12:41 +0530

    439-Div2 B
    Signed-off-by: Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>

--- Competitive-Programming/Codeforces/439-Div2/b.cpp
+++ Competitive-Programming/Codeforces/439-Div2/b.cpp
@@ -13,6 +13,10 @@ int main()
 {
         ll a, b, i;
         cin >> a >> b;
+        if (a == b) {
+                cout << "1\n";
+                return 0;
+        }
         int x = lastDigit(a + 1);
         for (i = a + 2; i <= b; i++) {
                 x = lastDigit((lastDigit(i) * lastDigit(x)));
