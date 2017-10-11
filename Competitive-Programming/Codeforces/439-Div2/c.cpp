dec4b2af6a277ff2e7ccde79fa8aaddb6dff7606
Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
2017-10-11 21:41:39 +0530

    freopen: Open from file when compiled with -DLOCAL
    Signed-off-by: Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>

--- Competitive-Programming/Codeforces/439-Div2/c.cpp
+++ Competitive-Programming/Codeforces/439-Div2/c.cpp
@@ -8,32 +8,12 @@ using namespace std;
 
 int main()
 {
+#ifdef LOCAL
+        // use it via g++ c.cpp -DLOCAL
+        freopen("inp.txt", "r", stdin);
+#endif
         int a, b, c;
         cin >> a >> b >> c;
 
-        int _a = a, _b = b, _c = c;
-        int i, total_nodes = 0;
-
-        for (i = 1; i <= a; i++) {
-                if (b > 0) {
-                        total_nodes += b;
-                        b--;
-                }
-
-                if (c > 0) {
-                        total_nodes += c;
-                        c--;
-                }
-        }
-
-        b = _b, c = _c;
-
-        for (i = 1; i <= c; i++) {
-                if (b > 0) {
-                        total_nodes += b;
-                        b--;
-                }
-        }
-        cout << (int)pow(2, total_nodes) % MOD << endl;
         return 0;
 }
